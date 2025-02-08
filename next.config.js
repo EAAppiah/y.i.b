0fcd14251581f1d91c7093728a9e4ce629dc85be
Captain K <kageappiah@outlook.com>
2025-02-08 22:20:11 +0000

    changes

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // your Next.js configuration options here
+    // for example:
+    reactStrictMode: true,
+    // add any other needed configuration
+  };
+  
+  module.exports = nextConfig;
