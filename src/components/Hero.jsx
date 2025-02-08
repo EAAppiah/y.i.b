3b3bc2a4c0552be7b46858f00ca95629fc7995cd
Captain K <kageappiah@outlook.com>
2025-02-08 22:01:16 +0000

    trials 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import DonationForm from './DonationForm';
+import DonationForm from '@/components/DonationForm';
 
 const Hero = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
